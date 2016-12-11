48d88909e342cbcf4a95edb88e2ed79bdedcd3d2
thefeeltrain <braydeneriq@gmail.com>
2016-12-11 11:54:13 -0800

    Cleanup time

--- js/cemu-ui.js
+++ js/cemu-ui.js
@@ -1,26 +1,26 @@
 var cemu = {
-    cemu_dir: "C:\\Users\\Ryan\\Desktop\\cemu\\cemu.exe",
-    game_dir: "E:\\Games\\cemu",
+    exe: "C:\\Users\\Ryan\\Desktop\\cemu\\cemu.exe",
+    gamedir: "E:\\Games\\cemu",
     initialize: function() {
-        if(localStorage.getItem("cemu_dir")) {
+        if (localStorage.getItem("exe")) {
             $('#navigation').addClass('top');
         }
-        var games = ["Hyrule Warriors","Donkey Kong Country Tropical Freeze","Mario Kart 8","Splatoon","Captain Toad Treasure Tracker","Super Mario Maker","Legend of Zelda Wind Waker HD"]; // For testing purposes
-        for(var i=0; i < games.length; i++) {
-            var c = Math.floor(Math.random()*10);
-            if(c < 3) {
+        var games = ["Hyrule Warriors", "Donkey Kong Country Tropical Freeze", "Mario Kart 8", "Splatoon", "Captain Toad Treasure Tracker", "Super Mario Maker", "Legend of Zelda Wind Waker HD"]; // For testing purposes
+        for (var i = 0; i < games.length; i++) {
+            var c = Math.floor(Math.random() * 10);
+            if (c < 3) {
                 c = "USA";
-            } else if(c >= 3 && c < 6) {
+            } else if (c >= 3 && c < 6) {
                 c = "EUR";
-            } else if(c >= 6 && c < 8) {
+            } else if (c >= 6 && c < 8) {
                 c = "GER";
             } else {
                 c = "JPN";
             }
-            $('<tr class="game" data-game-id="'+games[i]+'"><td class="icon"><img src="img/icon/'+games[i]+'.png"></td><td class="name">'+games[i]+'</td><td class="flag"><img src="img/'+c+'.png"> '+c+'</td><td class="status"></td></tr>').appendTo('#games');
+            $('<tr class="game" data-game-id="' + games[i] + '"><td class="icon"><img src="img/icon/' + games[i] + '.png"></td><td class="name">' + games[i] + '</td><td class="flag"><img src="img/' + c + '.png"> ' + c + '</td><td class="status"></td></tr>').appendTo('#games');
         }
         $('#directory').click(function() {
-            alert("Choose the exe directory.\nSet to " + cemu.cemu_dir + "."); //Choose directory here
+            alert("Choose the exe directory.\nSet to " + cemu.exe + "."); //Choose directory here
             cemu.save();
             $('#navigation').addClass('top');
         });
@@ -36,22 +36,22 @@ var cemu = {
         });
     },
     save: function() {
-        localStorage.setItem("cemu_dir", cemu.cemu_dir);
-        localStorage.setItem("game_dir", cemu.game_dir);
+        localStorage.setItem("exe", cemu.exe);
+        localStorage.setItem("dir", cemu.gamedir);
     },
     select: function(game) {
-        if(game != cemu.selected) {
+        if (game != cemu.selected) {
             cemu.selected = game;
-            $('#background').css('background-image','url("img/background/'+game+'.jpg")');
-            $('#game-icon').css('background-image','url("img/icon/'+game+'.png")');
+            $('#background').css('background-image', 'url("img/background/' + game + '.jpg")');
+            $('#game-icon').css('background-image', 'url("img/icon/' + game + '.png")');
             $('h2').text(game);
-            $('#game-playtime').text(Math.floor(Math.random()*20)+" hours played");
-            $('#game-lastplayed').text("Last played "+Math.floor(Math.random()*7)+" days ago");
+            $('#game-playtime').text(Math.floor(Math.random() * 20) + " hours played");
+            $('#game-lastplayed').text("Last played " + Math.floor(Math.random() * 7) + " days ago");
             $('.game').removeClass('selected');
         }
     },
     launch(game) {
-        alert("Launched "+game); //Launch function here
+        alert("Launched " + game); //Launch function here
     }
 };
 
