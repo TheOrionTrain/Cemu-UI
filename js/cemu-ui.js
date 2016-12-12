862237a4dc8e3e12b9d8fe90b016ff2a2547e4ab
Shadowfita <www.ryan.palmer@hotmail.com>
2016-12-12 20:46:55 +1000

    Sorting out Paths

--- js/cemu-ui.js
+++ js/cemu-ui.js
@@ -2,7 +2,7 @@ var cemu = {
     exe: "C:\\Users\\Ryan\\Desktop\\cemu\\cemu.exe",
     gamedir: "E:\\Games\\cemu",
     initialize: function() {
-        if (localStorage.getItem("exe")) {
+        /*if (localStorage.getItem("exe")) {
             $('#navigation').addClass('top');
         }
         var games = ["Hyrule Warriors", "Donkey Kong Country Tropical Freeze", "Mario Kart 8", "Splatoon", "Captain Toad Treasure Tracker", "Super Mario Maker", "Legend of Zelda Wind Waker HD"]; // For testing purposes
@@ -17,11 +17,9 @@ var cemu = {
             } else {
                 c = "JPN";
             }
-            $('<tr class="game" data-game-id="' + games[i] + '"><td class="icon"><img src="img/icon/' + games[i] + '.png"></td><td class="name">' + games[i] + '</td><td class="flag"><img src="img/' + c + '.png"> ' + c + '</td><td class="status"></td></tr>').appendTo('#games');
-        }
+            $('<tr class="game" data-game="" data-game-id="' + games[i] + '"><td class="icon"><img src="img/icon/' + games[i] + '.png"></td><td class="name">' + games[i] + '</td><td class="flag"><img src="img/' + c + '.png"> ' + c + '</td><td class="status"></td></tr>').appendTo('#games');
+        }*/
         $('#directory').click(function() {
-            alert("Choose the exe directory.\nSet to " + cemu.exe + "."); //Choose directory here
-            cemu.save();
             $('#navigation').addClass('top');
         });
         $('.game').hover(function() {
@@ -29,29 +27,22 @@ var cemu = {
             $(this).addClass('selected');
         });
         $('.game').dblclick(function() {
-            cemu.launch($(this).attr('data-game-id'));
+            app.launchGame($(this).attr('data-game-id'));
         });
         $('#launch').click(function() {
-            cemu.launch(cemu.selected);
+            app.launchGame(cemu.selected);
         });
-    },
-    save: function() {
-        localStorage.setItem("exe", cemu.exe);
-        localStorage.setItem("dir", cemu.gamedir);
     },
     select: function(game) {
         if (game != cemu.selected) {
             cemu.selected = game;
-            $('#background').css('background-image', 'url("img/background/' + game + '.jpg")');
-            $('#game-icon').css('background-image', 'url("img/icon/' + game + '.png")');
-            $('h2').text(game);
-            $('#game-playtime').text(Math.floor(Math.random() * 20) + " hours played");
-            $('#game-lastplayed').text("Last played " + Math.floor(Math.random() * 7) + " days ago");
+            $('#background').css('background-image', 'url("img/background/' + app.getPicture(game) + '.jpg")');
+            $('#game-icon').css('background-image', 'url("img/icon/' + app.getPicture(game) + '.png")');
+            $('h2').text(app.getName(game));
+            $('#game-playtime').text(app.getTimePlayed(game));
+            $('#game-lastplayed').text(app.getLastPlayed(game));
             $('.game').removeClass('selected');
         }
-    },
-    launch(game) {
-        alert("Launched " + game); //Launch function here
     }
 };
 
