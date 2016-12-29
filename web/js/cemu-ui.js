d5d8cfb98b2030571a688992aab7454c52ffda7d
thefeeltrain <braydeneriq@gmail.com>
2016-12-29 11:53:09 -0800

    Basic settings page

--- web/js/cemu-ui.js
+++ web/js/cemu-ui.js
@@ -1,38 +1,30 @@
 var cemu = {
     initialize: function() {
-        /*if (localStorage.getItem("exe")) {
-            $('#navigation').addClass('top');
-        }
-        var games = ["Hyrule Warriors", "Donkey Kong Country Tropical Freeze", "Mario Kart 8", "Splatoon", "Captain Toad Treasure Tracker", "Super Mario Maker", "Legend of Zelda Wind Waker HD"]; // For testing purposes
-        for (var i = 0; i < games.length; i++) {
-            var c = Math.floor(Math.random() * 10);
-            if (c < 3) {
-                c = "USA";
-            } else if (c >= 3 && c < 6) {
-                c = "EUR";
-            } else if (c >= 6 && c < 8) {
-                c = "GER";
-            } else {
-                c = "JPN";
-            }
-            $('<tr class="game" data-game="" data-game-id="' + games[i] + '"><td class="icon"><img src="img/icon/' + games[i] + '.png"></td><td class="name">' + games[i] + '</td><td class="flag"><img src="img/' + c + '.png"> ' + c + '</td><td class="status"></td></tr>').appendTo('#games');
-        }*/
         $('#directory').click(function() {
             $('#navigation').addClass('top');
+        });
+        $('#settings').click(function() {
+            $('#settings-container').addClass('center');
         });
         $('.game').hover(function() {
             cemu.select($(this).attr('data-game-id'));
             $(this).addClass('selected');
         });
-        $('.game').dblclick(function() {
-            app.launchGame($(this).attr('data-game-id'));
-        });
-        $('#launch').click(function() {
-            app.launchGame(cemu.selected);
-        });
+        $('[data-setting="theme"]').text("Dark");
+        $('[data-setting="background"]').text("On Hover");
+        if(app) {
+            $('[data-setting="game-dir"]').text(Settings.gameDirectory);
+            $('[data-setting="cemu-dir"]').text(Settings.cemuDirectory);
+            $('.game').dblclick(function() {
+                app.launchGame($(this).attr('data-game-id'));
+            });
+            $('#launch').click(function() {
+                app.launchGame(cemu.selected);
+            });
+        }
     },
     select: function(game) {
-        if (game != cemu.selected) {
+        if (game != cemu.selected && app) {
             cemu.selected = game;
             $('#background').css('background-image', 'url("img/background/' + app.getPicture(game) + '.jpg")');
             $('#game-icon').css('background-image', 'url("img/icon/' + app.getPicture(game) + '.png")');
