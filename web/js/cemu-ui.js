1d7c20ea5f0910eb3b751d8aeb55b71add91baad
Shadowfita <www.ryan.palmer@hotmail.com>
2017-01-08 02:24:57 +1000

    Events for Plugins

--- web/js/cemu-ui.js
+++ web/js/cemu-ui.js
@@ -7,7 +7,7 @@ var cemu = {
             $('#settings-container').addClass('center');
         });
         $('.game').hover(function() {
-            cemu.select($(this).attr('data-game-id'));
+            cemu.select({'game':$(this).attr('data-game-id'), 'titleId':(this).attr('data-title-id')});
             $(this).addClass('selected');
         });
         $('[data-setting="theme"]').text("Dark");
@@ -20,18 +20,20 @@ var cemu = {
             });
             $('#launch').click(function() {
                 app.launchGame(cemu.selected);
+				Events.OnGameLaunched(current.game);
             });
         }
     },
-    select: function(game) {
-        if (game != cemu.selected && app) {
-            cemu.selected = game;
-            $('#background').css('background-image', 'url("img/background/' + app.getPicture(game) + '.jpg")');
-            $('#game-icon').css('background-image', 'url("img/icon/' + app.getPicture(game) + '.png")');
-            $('h2').text(app.getName(game));
-            $('#game-playtime').text(app.getTimePlayed(game));
-            $('#game-lastplayed').text(app.getLastPlayed(game));
+    select: function(current) {
+        if (current.game != cemu.selected && app) {
+            cemu.selected = current.game;
+            $('#background').css('background-image', 'url("img/background/' + app.getPicture(current.game) + '.jpg")');
+            $('#game-icon').css('background-image', 'url("img/icon/' + app.getPicture(current.game) + '.png")');
+            $('h2').text(app.getName(current.game));
+            $('#game-playtime').text(app.getTimePlayed(current.game));
+            $('#game-lastplayed').text(app.getLastPlayed(current.game));
             $('.game').removeClass('selected');
+			Events.OnGameSelected(current.game, current.titleId);
         }
     }
 };
