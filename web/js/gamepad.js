c9e7447a04fcbe7a2ce2c915a1b5bdc158c6ef62
Shadowfita <www.ryan.palmer@hotmail.com>
2017-02-21 01:11:06 +1000

    Basic Controller Support
    Will be worked on some more in time.

--- web/js/gamepad.js
+++ web/js/gamepad.js
@@ -13,13 +13,15 @@ var Controller = {
         });
 
         gamepad.bind(Gamepad.Event.DISCONNECTED, function(device) {
-			console.log(device.id);
-            Controllers = $.grep(Controllers, function (value) {
-				return value != device;
-			});
-			
-			$('[data-setting="controller"] select option:not(:contains("None"))').remove();
-			Controllers.forEach((controller) => $('[data-setting="controller"] select').append('<option>' + controller.name + '</option>'));
+			if (!device.connected) {
+				console.log(device);
+				Controllers = $.grep(Controllers, function (value) {
+					return value != device;
+				});
+				
+				$('[data-setting="controller"] select option:not(:contains("None"))').remove();
+				Controllers.forEach((controller) => $('[data-setting="controller"] select').append('<option>' + controller.name + '</option>'));
+			}
         });
 
         gamepad.bind(Gamepad.Event.TICK, function(gamepads) {
@@ -28,10 +30,32 @@ var Controller = {
 
         gamepad.bind(Gamepad.Event.BUTTON_UP, function(e) {
 			console.log(e.gamepad.id);
+			if (!app.isInFocus())
+				return;
 			if (e.gamepad.name.includes($('[data-setting="controller"] select').val())) {
 				//do everything in here
 				console.log(e);
 				console.log(e.control);
+				switch (e.control) {
+					case "DPAD_DOWN":
+						$('tr.selected').next().mouseenter();//console.log("Move down.");
+						$('#games-container').scrollTo($('tr.selected'));
+					break;
+					
+					case "DPAD_UP":
+						$('tr.selected').prev().mouseenter();//console.log("Move up.");
+						$('#games-container').scrollTo($('tr.selected'));
+					break;
+					
+					case "FACE_1":
+						if ($('#navigation').hasClass('top'))
+							app.launchGame(cemu.selected.game);
+					break;
+					
+					case "START_FORWARD":
+						$('#navigation').addClass('top');
+					break;
+				}
 			}
         });
 		
@@ -42,8 +66,22 @@ var Controller = {
 		});
 
         gamepad.bind(Gamepad.Event.AXIS_CHANGED, function(e) {
+			if (!app.isInFocus() && $('#navigation').hasClass('top'))
+				return;
 			if (e.gamepad.name.includes($('[data-setting="controller"] select').val())) {
 				//do everything in here
+				//console.log(e);
+				switch (e.axis) {
+					case "LEFT_STICK_Y":
+						if (e.value == 1) {
+							$('tr.selected').next().mouseenter();//console.log("Move down.");
+							$('#games-container').scrollTo($('tr.selected'));
+						} else if (e.value == -1) {
+							$('tr.selected').prev().mouseenter();//console.log("Move up.");
+							$('#games-container').scrollTo($('tr.selected'));
+						}
+					break;
+				}
 			}
         });
 		
